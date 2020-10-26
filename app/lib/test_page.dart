a5c87642ff1315e14d64cdd6224fcc1349f96e4f
ronaldsin <ronald.sin@ontariotechu.net>
2020-10-26 16:35:56 -0400

    added test page

---
+++ app/lib/test_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TestPage extends StatelessWidget {
+  Color color;
+
+  TestPage(this.color);
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Container( 
+      color: color,
+    );
+    throw UnimplementedError();
+  }
+}
