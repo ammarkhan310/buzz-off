de789b80ac2ba8adde93695b25a7f2f9f71fff59
ronaldsin <ronald.sin@ontariotechu.net>
2020-10-26 19:05:09 -0400

    added log images

--- app/lib/statistics_page.dart
+++ app/lib/statistics_page.dart
@@ -30,16 +30,31 @@ class _StatisticsPageState extends State<StatisticsPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView.separated(
-        itemCount: biteHistoryList.length,
-        itemBuilder: (BuildContext context, int index){
-          return biteHistoryList[index].build();
-        }, 
-        separatorBuilder: (BuildContext context, int index) => Divider(), 
-        ) 
+      body: Column(
+        children: <Widget>[
+          biteLogger().build(),
+          Expanded(
+            child: ListView.separated(
+              itemCount: biteHistoryList.length,
+              itemBuilder: (BuildContext context, int index){
+                return biteHistoryList[index].build();
+              }, 
+              separatorBuilder: (BuildContext context, int index) => Divider(), 
+            )
+        )]
+      )
     );
 
     throw UnimplementedError();
+  }
+}
+
+class biteLogger{
+  //temp need to implement inveractivity 
+  String imageURL = 'assets/bitemap/noBite.png';
+
+  Widget build(){
+    return Image.asset(imageURL, height: 400, width: 500);
   }
 }
 
@@ -47,18 +62,22 @@ class biteHistory{
   String dateTime;
   int size; //1-10 bite serverity
   String location; //location of bite
+  String imageURL;
 
   biteHistory({
     this.dateTime,
     this.size,
     this.location
+    
   });
 
   Widget build(){
+    imageURL = 'assets/bitemap/' + '$location' + 'Bite.png';
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
       children: <Widget>[
+        Image.asset(imageURL, height: 100, width: 100),
         Text(dateTime),
         Text("Size: $size"),
         Text(location),
