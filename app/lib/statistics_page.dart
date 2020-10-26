126be8b502811f8cb6013eb9c93d82af8a6c10de
ronaldsin <ronald.sin@ontariotechu.net>
2020-10-26 16:35:44 -0400

    very basic stats page

--- app/lib/statistics_page.dart
+++ app/lib/statistics_page.dart
@@ -12,7 +12,57 @@ class StatisticsPage extends StatefulWidget {
 class _StatisticsPageState extends State<StatisticsPage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    List<biteHistory> biteHistoryList= <biteHistory>[
+      biteHistory( 
+        dateTime: '26-10-2020',
+        size: 5,
+        location: "arm",
+      ),
+
+      biteHistory( 
+        dateTime: '31-05-2019',
+        size: 3,
+        location: "leg",
+      ),
+    ];
+  
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.separated(
+        itemCount: biteHistoryList.length,
+        itemBuilder: (BuildContext context, int index){
+          return biteHistoryList[index].build();
+        }, 
+        separatorBuilder: (BuildContext context, int index) => Divider(), 
+        ) 
+    );
+
     throw UnimplementedError();
+  }
+}
+
+class biteHistory{
+  String dateTime;
+  int size; //1-10 bite serverity
+  String location; //location of bite
+
+  biteHistory({
+    this.dateTime,
+    this.size,
+    this.location
+  });
+
+  Widget build(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+      children: <Widget>[
+        Text(dateTime),
+        Text("Size: $size"),
+        Text(location),
+      ],
+      );
   }
 }
